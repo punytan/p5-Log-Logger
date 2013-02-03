f3a0e9c2c95d570b3b934d74cfd32f4957cb03c6
punytan <punytan@gmail.com>
2013-02-03 20:29:18 +0900

    initial commit

---
+++ lib/Log/Logger.pm
@@ -0,0 +1,32 @@
+package Log::Logger;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Log::Logger -
+
+=head1 SYNOPSIS
+
+  use Log::Logger;
+
+=head1 DESCRIPTION
+
+Log::Logger is
+
+=head1 AUTHOR
+
+punytan E<lt>punytan@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
