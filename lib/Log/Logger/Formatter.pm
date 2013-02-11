a42f265449fa0e9b4d6e0322e83f998afe5e8a27
punytan <punytan@gmail.com>
2013-02-12 02:29:57 +0900

    flexible line formatter #10

--- lib/Log/Logger/Formatter.pm
+++ lib/Log/Logger/Formatter.pm
@@ -13,12 +13,36 @@ sub new {
     my $ts_format = $args{timestamp_format} // '%Y-%m-%dT%H:%M:%S';
     my $auto_newline  = $args{auto_newline} // 1;
     my $caller_level  = $args{caller_level} // 4;
+    my $message_layout = $args{message_layout} // "%I, [%T #%P] %x -- %p: %m at %f line %l";
 
     bless {
         timestamp_format => $ts_format,
         auto_newline     => $auto_newline,
         caller_level     => $caller_level,
+        message_layout   => $message_layout,
     }, $class;
+}
+
+sub render_layout {
+    my ($self, $level, $message) = @_;
+
+    my $caller = $self->caller;
+    my %map = (
+        I => sub { substr $self->level_name($level), 0, 1 },
+        T => sub { $self->timestamp },
+        P => sub { $self->pid },
+        L => sub { $self->level_name($level) },
+        x => sub { sprintf "%5s", $self->level_name($level) },
+        p => sub { $caller->{package} },
+        m => sub { $message },
+        f => sub { $caller->{filename} },
+        l => sub { $caller->{line} },
+    );
+
+    my $layout = $self->{message_layout};
+    $layout =~ s/\%([ITPLpmflx])/$map{$1}->()/ge;
+
+    $layout;
 }
 
 sub timestamp {
@@ -43,7 +67,7 @@ sub caller   {
     for (my $i = 0; ($package, $filename, $line) = caller $i; $i++) {
         last if $package !~ /^Log::Logger/;
     }
-    return [$package, $filename, $line];
+    return { package => $package, filename => $filename, line => $line };
 }
 
 1;
--- lib/Log/Logger/Formatter/Default.pm
+++ lib/Log/Logger/Formatter/Default.pm
@@ -31,11 +31,7 @@ sub format {
         ? Term::ANSIColor::colored($self->{color}{$level_name}, "@args")
         : "@args";
 
-    my $caller = $self->caller;
-    my $log = sprintf "%s, [%s #%d] %5s -- %s: %s at %s line %s",
-        $severity_id, $self->timestamp, $self->pid, $level_name,
-        $caller->[0], $message, $caller->[1], $caller->[2];
-
+    my $log   = $self->render_layout($level, $message);
     if ($self->{auto_newline}) {
         chomp $log;
         return "$log\n";
