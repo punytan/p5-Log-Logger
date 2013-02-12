92957bfc783cadf30d8125f0b103246e20d7b8d4
punytan <punytan@gmail.com>
2013-02-13 01:28:59 +0900

    add docs for Log::Logger::Handler #9

--- lib/Log/Logger/Handler.pm
+++ lib/Log/Logger/Handler.pm
@@ -30,3 +30,33 @@ sub write { }
 1;
 __END__
 
+=head1 NAME
+
+Log::Logger::Handler - Base class for Log::Logger's output handler
+
+=head1 METHODS
+
+=head2 C<formatter>
+
+Accessor to the formatter instance.
+
+=head2 C<write($level, @args)>
+
+The log level condition is sufficient, logger calls C<write> method with arguments (C<$level> and C<@args>).
+
+=head1 AUTHOR
+
+punytan E<lt>punytan@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+L<Log::Logger::Handler::Screen>
+L<Log::Logger::Handler::File>
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
